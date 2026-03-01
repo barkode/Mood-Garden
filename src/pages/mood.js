5163db9267c351d0045a169203c4000979e089b3
barkode <konstantinks@gmail.com>
2026-03-01 00:57:29 +0000

    Implement mood page rendering function to display logging page header

---
+++ src/pages/mood.js
@@ -0,0 +1,7 @@
+export function renderMoodPage() {
+    const container = document.getElementById('page-log')
+    if(!container) return;
+    container.innerHTML = `
+    <h1> Logging Page</h1>
+  `
+}
